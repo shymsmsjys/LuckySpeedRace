a41f6d1cc50e28cb5faedd66f5ef11faedaa94d0
shcha <yoonseo.cha.co@gmail.com>
2020-11-05 17:12:06 +0900

    display board image

--- src/com/ssk/game/Board.java
+++ src/com/ssk/game/Board.java
@@ -1,7 +1,34 @@
 package com.ssk.game;
 
-public class Board {
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private ImageIcon imgBoard;
 	Horse[] horses;
 	
+	public Board() {
+		init();
+	}
+	
+	private void init() {
+		imgBoard = new ImageIcon("res/img_board.png");
+		JLabel board = new JLabel(imgBoard);
+		add(board);
+	}
+//
+//	@Override
+//	public void paint(Graphics arg0) {
+//		// TODO Auto-generated method stub
+//		super.paint(arg0);
+//		arg0.drawImage(imgBoard, 0, 0, null);
+//	}
 
 }
--- src/com/ssk/game/Main.java
+++ src/com/ssk/game/Main.java
@@ -1,16 +1,31 @@
 package com.ssk.game;
 
+import javax.swing.JFrame;
+
 public class Main {
+	private JFrame frame;
 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new Main();
+		Main window = new Main();
+		window.frame.setVisible(true);
 	}
 	
 	public Main() {
+		init();
+		
 		GameManager gm = new GameManager();
 		gm.start();
+	}
+	
+	private void init() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(300, 20, 790, 720);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Board board = new Board();
+		frame.add(board);
 	}
 
 }
